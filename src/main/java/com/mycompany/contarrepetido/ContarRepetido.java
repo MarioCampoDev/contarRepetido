11d53f7b581ba9f3fcdc384590310f63a9fbf840
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-09-22 22:55:12 -0500

    initial commit

---
+++ src/main/java/com/mycompany/contarrepetido/ContarRepetido.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contarrepetido;
+
+/**
+ *
+ * @author mario
+ */
+public class ContarRepetido {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
